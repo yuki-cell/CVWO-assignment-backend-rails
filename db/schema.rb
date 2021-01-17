ca39ffaef2c8d30dfddad6acffa0d0052f6e5e75
Sushi <54584459+yuki-cell@users.noreply.github.com>
2021-01-17 18:09:30 +0800

    update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_13_023033) do
+ActiveRecord::Schema.define(version: 2021_01_17_090657) do
 
   create_table "tags", force: :cascade do |t|
     t.string "name"
